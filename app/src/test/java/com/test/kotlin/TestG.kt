d1e23beff648e96157667820fd825e5f9592f04f
lychee <hanliuxin123@hotmail.com>
2018-03-08 16:17:25 +0800

    add:厉害了我的kotlin

--- app/src/test/java/com/test/kotlin/TestG.kt
+++ app/src/test/java/com/test/kotlin/TestG.kt
@@ -1,11 +1,36 @@
 package com.test.kotlin
 
+import com.test.kotlin.Test.A
 import org.junit.Test
+import java.util.*
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
 
 /**
  * Created by lychee on 18-3-7.
  */
+
+
 class TestG {
+
+    val lazyValue: Double by lazy {
+        Math.random() * 10
+    }
+
+    var mutableV: Int by Delegates.observable(0) { pro, old, new ->
+        println("$pro,$old,$new")
+    }
+
+    var v2: Int by Delegates.vetoable(-1) { property, oldValue, newValue ->
+        newValue > 10
+    }
+
+    class Go<T : Test2.F>(val t: T) {
+        fun fun1() {
+            print(t)
+        }
+    }
+
     @Test
     fun test1() {
         val op = object : O("wow"), P {
@@ -21,6 +46,55 @@ class TestG {
         bit.dig(20.0)
         bit.show()
 
+        val impl = Impl()
+        Laj(impl).dig(233.0)
+        Laj(impl).show()
+
+        val zs = Zs()
+        println(zs.location)
+        zs.location = "wow"
+
+        println(lazyValue)
+        println(lazyValue)
+
+        mutableV = 10
+
+        v2 = 11
+        println(v2)
+
+        val list = intArrayOf(1, 2, 3, 4, 5, 6, 0)
+        longF(*list)
+
+        val go = Go<Test2.F>(Test2.A())
+
+//        fun go1(i: Int): (index: Int) -> Int = i * i
+
+        var l = List<Number>(5, { i -> i * i })
+        val ll = List<Int>(5, { it * it })
+        val list1: MutableCollection<Int>? = null
+        val list2: MutableCollection<Number>? = null
+
+//        testList(list1 ?: mutableListOf())
+
+        val map = mapOf<String, String>("1" to "11", "2" to "22")
+        val listFmap = map.map { it.value }
+        println(listFmap)
+
+        val r = lychee<String>(mutableListOf("1", "s", "gg", "wp"), { x, y -> x + y == "ss" })
+        println(r ?: "nope")
+    }
+
+    fun testList(mu: MutableCollection<Number>) {
+
+    }
+
+    fun <T> lychee(collection: MutableCollection<out T>, wow: (T, T) -> Boolean): T? {
+        var w: T? = null
+        for (it in collection) {
+            if (w != null && wow(w, it))
+                w = it
+        }
+        return w
     }
 
     open class O(val x: String) {
@@ -50,6 +124,12 @@ class TestG {
         }
     }
 
+    class Laj(bit: Bit) : Bit by bit {
+        override fun dig(cm: Double) {
+            println("laj proxy dig:$cm")
+        }
+    }
+
     class Hlx(bit: Bit) : Bit by bit {
         override fun dig(cm: Double) {
             println("proxy dig:$cm")
@@ -60,4 +140,24 @@ class TestG {
         }
     }
 
+    class Zs {
+        var location: String by HighSchool()
+    }
+
+    class HighSchool {
+        operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+            return "$thisRef, thank you for delegating '${property.name}' to me!"
+        }
+
+        operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+            println("$value has been assigned to '${property.name} in $thisRef.'")
+        }
+    }
+
+    fun longF(vararg t: Int) {
+        for (i in t)
+            print(i)
+
+    }
+//    class Lyl(val map:Map)
 }
