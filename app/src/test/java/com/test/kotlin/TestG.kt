446bca4ffdbee47bdd61c87e8379ad77b6ac33ca
lychee <hanliuxin123@hotmail.com>
2018-03-08 10:20:47 +0800

    add:proxy mode test

--- app/src/test/java/com/test/kotlin/TestG.kt
+++ app/src/test/java/com/test/kotlin/TestG.kt
@@ -8,10 +8,56 @@ import org.junit.Test
 class TestG {
     @Test
     fun test1() {
+        val op = object : O("wow"), P {
+            override fun ed() = "jj"
+        }
+        val wy = object {
+            var x: Int = 1
+            var y: Int = 2
+        }
+        println("${wy.x},${wy.x.javaClass}")
+
+        val bit: Bit = Hlx(Impl())
+        bit.dig(20.0)
+        bit.show()
 
     }
 
-    enum class L {
-        A, B, C
+    open class O(val x: String) {
+        public open var y = x
+
+        companion object fun1 {
+            fun fun1() {}
+        }
     }
+
+    interface P {
+        fun ed() = "kk"
+    }
+
+    interface Bit {
+        fun dig(cm: Double)
+        fun show()
+    }
+
+    class Impl() : Bit {
+        override fun dig(cm: Double) {
+            println("origin dig:$cm")
+        }
+
+        override fun show() {
+            println("origin show")
+        }
+    }
+
+    class Hlx(bit: Bit) : Bit by bit {
+        override fun dig(cm: Double) {
+            println("proxy dig:$cm")
+        }
+
+        override fun show() {
+            println("proxy show")
+        }
+    }
+
 }
